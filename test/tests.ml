5ac8f780cbb797dea037cc498e5c5c4dc4f760e3
Jeremie Dimino <jdimino@janestreet.com>
2018-03-29 16:20:55 -0400

    Setup tests

--- test/tests.ml
+++ test/tests.ml
@@ -1,15 +1,14 @@
-open! Core
-open Expect_test_helpers_kernel
-
-(* CR-someday jdimino for jdimino: if we don't a reference to [Core.Unix], the
-   sexpifier for [Unix.Unix_error] are not registered *)
-let _ = ignore (Unix.Unix_error (ENOENT, "", ""))
+let show_raise f =
+  try
+    ignore (f () : int)
+  with exn ->
+    Printf.printf "raised %s" (Printexc.to_string exn)
 
 let%expect_test "non-existing program" =
   show_raise (fun () ->
     Spawn.spawn () ~prog:"/doesnt-exist" ~argv:["blah"]);
   [%expect {|
-    (raised (Unix.Unix_error "No such file or directory" execve /doesnt-exist))
+    raised Unix.Unix_error(Unix.ENOENT, "execve", "/doesnt-exist")
   |}]
 
 let%expect_test "non-existing dir" =
@@ -17,19 +16,19 @@ let%expect_test "non-existing dir" =
     Spawn.spawn () ~prog:"/bin/true" ~argv:["true"]
       ~cwd:(Path "/doesnt-exist"));
   [%expect {|
-    (raised (Unix.Unix_error "No such file or directory" chdir /doesnt-exist))
+    raised Unix.Unix_error(Unix.ENOENT, "chdir", "/doesnt-exist")
   |}]
 
-let wait n =
-  Pid.of_int n
-  |> Unix.waitpid
-  |> Unix.Exit_or_signal.or_error
-  |> ok_exn
+let wait pid =
+  match snd (Unix.waitpid [] pid) with
+  | WEXITED   0 -> ()
+  | WEXITED   n -> Printf.ksprintf failwith "exited with code %d" n
+  | WSIGNALED n -> Printf.ksprintf failwith "got signal %d" n
+  | WSTOPPED  _ -> assert false
 
 let%expect_test "cwd:Fd" =
-  let fd = Unix.openfile "/tmp" ~mode:[O_RDONLY] in
-  Spawn.spawn () ~prog:"/bin/pwd" ~argv:["pwd"] ~cwd:(Fd fd)
-  |> wait;
+  let fd = Unix.openfile "/tmp" [O_RDONLY] 0 in
+  wait (Spawn.spawn () ~prog:"/bin/pwd" ~argv:["pwd"] ~cwd:(Fd fd));
   Unix.close fd;
   [%expect {|
     /tmp
@@ -39,11 +38,10 @@ let%expect_test "cwd:Fd (invalid)" =
   show_raise (fun () ->
     Spawn.spawn () ~prog:"/bin/pwd" ~argv:["pwd"] ~cwd:(Fd Unix.stdin));
   [%expect {|
-    (raised (Unix.Unix_error "Not a directory" fchdir ""))
+    raised Unix.Unix_error(Unix.ENOTDIR, "fchdir", "")
   |}]
 
 let%expect_test "inheriting stdout with close-on-exec set" =
   Unix.set_close_on_exec Unix.stdout;
-  Spawn.spawn () ~prog:"/bin/echo" ~argv:["echo"; "hello world"]
-  |> wait;
+  wait (Spawn.spawn () ~prog:"/bin/echo" ~argv:["echo"; "hello world"]);
   [%expect {| hello world |}]
