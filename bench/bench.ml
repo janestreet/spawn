343eb6a8d77f9181e9e4b85a4d524ac985b9cb5b
Jeremie Dimino <jeremie@dimino.org>
2021-03-29 09:49:54 +0100

    Enable formatting
    Signed-off-by: Jeremie Dimino <jeremie@dimino.org>

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,8 +1,7 @@
 let%bench "exec true with Spawn.spawn" =
-  Spawn.spawn () ~prog:"/bin/true" ~argv:["true"]
-  |> Unix.waitpid []
+  Spawn.spawn () ~prog:"/bin/true" ~argv:[ "true" ] |> Unix.waitpid []
 
 let%bench "exec true with Caml.Unix.create_process" =
-  Unix.create_process "/bin/true" [|"true"|] Unix.stdin Unix.stdout Unix.stderr
+  Unix.create_process "/bin/true" [| "true" |] Unix.stdin Unix.stdout
+    Unix.stderr
   |> Unix.waitpid []
-
